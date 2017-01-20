20c30cc2dd24410d95dfa2b18ab8ec957267cb9c
David E. Reese <dereese_cei@yahoo.com>
2017-01-20 06:16:14 -0600

    Added ComplexMatrix.set() for Programming Drill 2.1.1.

--- src/qcfcs_math/ComplexMatrix.java
+++ src/qcfcs_math/ComplexMatrix.java
@@ -28,7 +28,8 @@ import java.util.ArrayList;
 // along with AsianFlashCard.  If not, see <http://www.gnu.org/licenses/>.
 //
 // History:
-//      20170118    D.E. Reese          Creation (Programming Drill 2.1.1)
+//      20170118    D.E. Reese          Creation (Programming Drill 2.1.1).
+//      20170119    D.E. Reese          Added set () (Programming Drill 2.1.1).
 //
 
 public class ComplexMatrix
@@ -102,7 +103,7 @@ public class ComplexMatrix
      * @param row   Row of the desired element. Note that this is 0-based.
      * @param column    Column of the desired element. Note that this is 0-based.
      * @return  Element at this[row][column]
-     * @throws IndexOutOfBoundsException    Thrown if row or column is out of bounds.
+     * @throws IndexOutOfBoundsException    Thrown if row or column are out of bounds.
      */
     public Complex get(int row, int column) throws IndexOutOfBoundsException
     {
@@ -110,5 +111,25 @@ public class ComplexMatrix
         if ((column < 0) || (column >= numColumns)) throw new IndexOutOfBoundsException("column < 0 or column >= this.numColumns");
 
         return theData[row][column];
+    }
+
+    /**
+     * This method sets an element of the matrix to a new value.
+     * @param row   Row of the element to be set. Note that this is 0-based.
+     * @param column    Column of the element to be set. Note that this is 0-based.
+     * @param element   New value of the specified element.
+     * @return  Previous value of the element.
+     * @throws IndexOutOfBoundsException    Thrown if row or column are out of bounds.
+     * @throws IllegalArgumentException     Thrown if element is null.
+     */
+    public Complex set(int row, int column, Complex element) throws IndexOutOfBoundsException, IllegalArgumentException
+    {
+        if ((row < 0) || (row >= numRows)) throw new IndexOutOfBoundsException("row < 0 or row >= this.numRows.");
+        if ((column < 0) || (column >= numColumns)) throw new IndexOutOfBoundsException("column < 0 or column >= this.numColumns.");
+        if (element == null) throw new IllegalArgumentException("element is null.");
+
+        Complex currentValue = theData[row][column];
+        theData[row][column] = element;
+        return currentValue;
     }
 }
