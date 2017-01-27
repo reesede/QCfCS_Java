b27b522d304fc226993a427d7b31e568b3db02fc
David E. Reese <dereese_cei@yahoo.com>
2017-01-27 06:09:17 -0600

    Programming Drill 2.1.1 - Added additional tests to ComplexMatrixTest.setAndGet() and tests to ComplexMatrixTest.add().

--- test/qcfcs_math/ComplexMatrixTest.java
+++ test/qcfcs_math/ComplexMatrixTest.java
@@ -34,6 +34,7 @@ import static org.junit.jupiter.api.Assertions.*;
 // History:
 //      20170122    D.E. Reese          Creation (Programming Drill 1.2.1)
 //      20170126    D.E. Reese          Merged get() and set() into setAndGet() and added tests to stub.
+//      20170127    D.E. Reese          Added additional tests to setAndGet() and added tests to add() stub.
 //
 
 class ComplexMatrixTest
@@ -54,6 +55,50 @@ class ComplexMatrixTest
     void setAndGet()
     {
         ComplexMatrix theMatrix = new ComplexMatrix(2,2);
+        Complex theComplex;
+
+        theComplex = ComplexMatrix.get(theMatrix, 0, 0);
+        assertEquals(0.0, theComplex.getReal());
+        assertEquals(0.0, theComplex.getImag());
+
+        theComplex = ComplexMatrix.get(theMatrix, 0, 1);
+        assertEquals(0.0, theComplex.getReal());
+        assertEquals(0.0, theComplex.getImag());
+
+        theComplex = ComplexMatrix.get(theMatrix, 1, 0);
+        assertEquals(0.0, theComplex.getReal());
+        assertEquals(0.0, theComplex.getImag());
+
+        theComplex = ComplexMatrix.get(theMatrix, 1, 1);
+        assertEquals(0.0, theComplex.getReal());
+        assertEquals(0.0, theComplex.getImag());
+
+        ComplexMatrix.set(theMatrix,0,0, new Complex(1.0, 1.0));
+        theComplex = ComplexMatrix.get(theMatrix,0,0);
+        assertEquals(1.0, theComplex.getReal());
+        assertEquals(1.0, theComplex.getImag());
+
+        theMatrix.set(0,1, (double)2.0);
+        theComplex = ComplexMatrix.get(theMatrix,0,1);
+        assertEquals(2.0, theComplex.getReal());
+        assertEquals(0.0, theComplex.getImag());
+
+        theMatrix.set(1,0,(float)3.0);
+        theComplex = ComplexMatrix.get(theMatrix,1,0);
+        assertEquals(3.0, theComplex.getReal());
+        assertEquals(0.0, theComplex.getImag());
+
+        theMatrix.set(1,1,4);
+        theComplex = ComplexMatrix.get(theMatrix,1,1);
+        assertEquals(4.0, theComplex.getReal());
+        assertEquals(0.0, theComplex.getImag());
+
+        theMatrix.set(0,0, new PolarCoordinate(4.0, Math.PI/4.0));
+        theComplex = ComplexMatrix.get(theMatrix,0,0);
+        assertEquals(2.0*Math.sqrt(2.0), theComplex.getReal(), 0.00000001);
+        assertEquals(2.0*Math.sqrt(2.0), theComplex.getImag(), 0.00000001);
+
+        theMatrix = new ComplexMatrix(2,2);
         assertEquals(0.0, theMatrix.get(0,0).getReal());
         assertEquals(0.0, theMatrix.get(0,0).getImag());
         assertEquals(0.0, theMatrix.get(0,1).getReal());
@@ -88,6 +133,115 @@ class ComplexMatrixTest
     @Test
     void add()
     {
+        // Test static operations.
+
+        ComplexMatrix matrix1 = new ComplexMatrix(2,2);
+        ComplexMatrix matrix2 = new ComplexMatrix(2,2);
+
+        matrix1.set(0,0, new Complex(1.0, 1.0));
+        matrix1.set(0,1, new Complex (2.0, 2.0));
+        matrix1.set(1,0, new Complex (3.0, -2.0));
+        matrix1.set(1,1, 5.0);
+
+        ComplexMatrix sum = ComplexMatrix.add(matrix1, matrix2);
+        assertEquals(1.0, sum.get(0,0).getReal());
+        assertEquals(1.0, sum.get(0,0).getImag());
+        assertEquals(2.0, sum.get(0,1).getReal());
+        assertEquals(2.0, sum.get(0,1).getImag());
+        assertEquals(3.0, sum.get(1,0).getReal());
+        assertEquals(-2.0, sum.get(1,0).getImag());
+        assertEquals(5.0, sum.get(1,1).getReal());
+        assertEquals(0.0, sum.get(1,1).getImag());
+
+        matrix2.set(0,0, new Complex(-1.0, -1.0));
+        matrix2.set(0,1, new Complex (-1.0, -1.0));
+        matrix2.set(1,0, new Complex (-1.0, -1.0));
+        matrix2.set(1,1, new Complex (-1.0, -1.0));
+
+        sum = ComplexMatrix.add(matrix1, matrix2);
+        assertEquals(0.0, sum.get(0,0).getReal());
+        assertEquals(0.0, sum.get(0,0).getImag());
+        assertEquals(1.0, sum.get(0,1).getReal());
+        assertEquals(1.0, sum.get(0,1).getImag());
+        assertEquals(2.0, sum.get(1,0).getReal());
+        assertEquals(-3.0, sum.get(1,0).getImag());
+        assertEquals(4.0, sum.get(1,1).getReal());
+        assertEquals(-1.0, sum.get(1,1).getImag());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            ComplexMatrix.add(new ComplexMatrix(2,2), new ComplexMatrix(2,1));
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            ComplexMatrix.add(new ComplexMatrix(2,2), new ComplexMatrix(1,2));
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            ComplexMatrix.add(new ComplexMatrix(3,2), new ComplexMatrix(2,1));
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            ComplexMatrix.add(new ComplexMatrix(2,2), null);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            ComplexMatrix.add(null, new ComplexMatrix(2,1));
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            ComplexMatrix.add(null, null);
+        });
+
+        // Test instance operation.
+
+        matrix1 = new ComplexMatrix(2,2);
+        matrix2 = new ComplexMatrix(2,2);
+
+        matrix1.set(0,0, new Complex(1.0, 1.0));
+        matrix1.set(0,1, new Complex (2.0, 2.0));
+        matrix1.set(1,0, new Complex (3.0, -2.0));
+        matrix1.set(1,1, 5.0);
+
+        sum = matrix1.add(matrix2);
+        assertEquals(1.0, sum.get(0,0).getReal());
+        assertEquals(1.0, sum.get(0,0).getImag());
+        assertEquals(2.0, sum.get(0,1).getReal());
+        assertEquals(2.0, sum.get(0,1).getImag());
+        assertEquals(3.0, sum.get(1,0).getReal());
+        assertEquals(-2.0, sum.get(1,0).getImag());
+        assertEquals(5.0, sum.get(1,1).getReal());
+        assertEquals(0.0, sum.get(1,1).getImag());
+
+        matrix2.set(0,0, new Complex(-1.0, -1.0));
+        matrix2.set(0,1, new Complex (-1.0, -1.0));
+        matrix2.set(1,0, new Complex (-1.0, -1.0));
+        matrix2.set(1,1, new Complex (-1.0, -1.0));
+
+        sum = matrix1.add(matrix2);
+        assertEquals(0.0, sum.get(0,0).getReal());
+        assertEquals(0.0, sum.get(0,0).getImag());
+        assertEquals(1.0, sum.get(0,1).getReal());
+        assertEquals(1.0, sum.get(0,1).getImag());
+        assertEquals(2.0, sum.get(1,0).getReal());
+        assertEquals(-3.0, sum.get(1,0).getImag());
+        assertEquals(4.0, sum.get(1,1).getReal());
+        assertEquals(-1.0, sum.get(1,1).getImag());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new ComplexMatrix(2,2).add(new ComplexMatrix(2,1));
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new ComplexMatrix(2,2).add(new ComplexMatrix(1,2));
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new ComplexMatrix(2,2).add(new ComplexMatrix(3,1));
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new ComplexMatrix(2,2).add(null);
+        });
 
     }
 
