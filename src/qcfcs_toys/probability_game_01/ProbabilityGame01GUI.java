1de5ef75b3b42668b8690e274b331e5dff830d2c
David E. Reese <dereese_cei@yahoo.com>
2017-02-19 07:15:24 -0600

    Programming Drill 3.1.1 - Created new branch to experiment with Intellij IDEA GUI creator for program.

---
+++ src/qcfcs_toys/probability_game_01/ProbabilityGame01GUI.java
@@ -0,0 +1,14 @@
+package qcfcs_toys.probability_game_01;
+
+import javax.swing.*;
+
+/**
+ * Created by reesede on 2/19/17.
+ */
+public class ProbabilityGame01GUI
+{
+    private JPanel panel1;
+    private JPanel GameTypePanel;
+    private JPanel GameMatrixPanel;
+    private JPanel ControlPanel;
+}
