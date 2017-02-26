7d782b7305ea8c1f80de7952ecafbef8451d76b0
David E. Reese <dereese_cei@yahoo.com>
2017-02-26 11:51:10 -0600

    Programming Drill 3.1.1 - Added code to set up matrix and vector tables (incomplete)

--- src/qcfcs_toys/probability_game_01/ProbabilityGame01GUI.java
+++ src/qcfcs_toys/probability_game_01/ProbabilityGame01GUI.java
@@ -1,8 +1,13 @@
 package qcfcs_toys.probability_game_01;
 
 import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.NumberFormat;
 
 /**
  * Created by reesede on 2/19/17.
@@ -27,12 +32,22 @@ public class ProbabilityGame01GUI
     /**
      * Width of window.
      */
-    private static final int width = 800;
+    private static final int width = 1000;
 
     /**
      * Height of window.
      */
     private static final int height = 600;
+
+    /**
+     * Initial number of states in the game.
+     */
+    private static final int initialNumberOfStates = 5;
+
+    /**
+     * Preferred width of columns.
+     */
+    private static final int tableColumnWidth = 50;
 
     private JFrame mainFrame;
     private JPanel panel1;
@@ -62,27 +77,15 @@ public class ProbabilityGame01GUI
     {
         // Set up main frame.
 
-        mainFrame = new JFrame();
-        mainFrame.setSize(width, height);
-        mainFrame.setLocation(xLoc,yLoc);
-        mainFrame.setResizable(true);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setContentPane(panel1);
+        initializeMainFrame();
 
-        // Make the window visible.
+        // Create the game type radio button group.
 
-        mainFrame.pack();
-        mainFrame.setVisible(true);
+        initializeGameTypeButtonGroup();
 
-        // Create the game type radio buttons.
+        // Initialize the transition matrix and state vector.
 
-        gameTypeButtonGroup = new ButtonGroup();
-        gameTypeButtonGroup.add(booleanGameButton);
-        gameTypeButtonGroup.add(realGameButton);
-        gameTypeButtonGroup.add(complexGameButton);
-        booleanGameButton.setSelected(true);
-        realGameButton.setSelected(false);
-        complexGameButton.setSelected(false);
+        initializeTransitionMatrixAndStateVector();
 
         quitButton.addActionListener(new ActionListener()
         {
@@ -101,5 +104,67 @@ public class ProbabilityGame01GUI
 
             }
         });
+
+        // Make the window visible.
+
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+    }
+
+    /**
+     * This method initializes the frame for the program GUI.
+     */
+    private void initializeMainFrame ()
+    {
+        mainFrame = new JFrame();
+        mainFrame.setSize(width, height);
+        mainFrame.setLocation(xLoc,yLoc);
+        mainFrame.setResizable(true);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setContentPane(panel1);
+    }
+
+    /**
+     * This method initializes the game type radio button group.
+     */
+    private void initializeGameTypeButtonGroup()
+    {
+        gameTypeButtonGroup = new ButtonGroup();
+        gameTypeButtonGroup.add(booleanGameButton);
+        gameTypeButtonGroup.add(realGameButton);
+        gameTypeButtonGroup.add(complexGameButton);
+        booleanGameButton.setSelected(true);
+        realGameButton.setSelected(false);
+        complexGameButton.setSelected(false);
+    }
+
+    private void initializeTransitionMatrixAndStateVector()
+    {
+        DefaultTableModel transitionTableModel = new DefaultTableModel(initialNumberOfStates,initialNumberOfStates);
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+
+        for(int i = 0; i < initialNumberOfStates; i++)
+            for(int j = 0; j < initialNumberOfStates; j++)
+            {
+                String theValue = "0";
+                if (i == j)
+                    theValue = "1";
+                transitionTableModel.setValueAt(theValue,i,j);
+            }
+
+        transitionMatrixTable.setTableHeader(null);
+        transitionMatrixTable.setModel(transitionTableModel);
+        for(int i = 0; i < transitionMatrixTable.getColumnCount(); i++)
+        {
+            transitionMatrixTable.getColumnModel().getColumn(i).setPreferredWidth(tableColumnWidth);
+            transitionMatrixTable.getColumnModel().getColumn(i).setCellRenderer( centerRenderer );
+        }
+
+        // Set up the initial state vector.
+
+        DefaultTableModel vectorTableModel = new DefaultTableModel(initialNumberOfStates,1);
+
+
     }
 }
