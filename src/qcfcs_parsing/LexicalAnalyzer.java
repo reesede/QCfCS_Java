42fef727a263e68bca585c184b84309935582a98
David E. Reese <dereese_cei@yahoo.com>
2017-03-28 06:13:30 -0500

    Added constructor to LexicalAnalyzer.

--- src/qcfcs_parsing/LexicalAnalyzer.java
+++ src/qcfcs_parsing/LexicalAnalyzer.java
@@ -1,5 +1,7 @@
 package qcfcs_parsing;
 
+import java.util.ArrayList;
+
 /**
  * This class implements a lexical analyzer to convert a string into a list of tokens.
  * Created by reesede on 3/26/2017.
@@ -27,6 +29,7 @@ package qcfcs_parsing;
 //
 // History:
 //      20170326    D.E. Reese          Creation.
+//      20170328    D.E. Reese          Created constructor and stub for analyseString().
 
 public class LexicalAnalyzer
 {
@@ -90,5 +93,71 @@ public class LexicalAnalyzer
      */
     private static final int integerBinary = 3;
 
+    /**
+     * State of analysis.
+     */
+    private int lexicalState;
+
+    /**
+     * Location in the string to be analysed.
+     */
+    private int stringLocation;
+
+    /**
+     * Length of string to analyse.
+     */
+    private int stringLength;
+
+    /**
+     * Private copy of the string to analyse.
+     */
+    private String workString;
+
+    /**
+     * List of tokens found.
+     */
+    private ArrayList<LexicalToken> tokenList;
+
+    /**
+     * List of strings containing errors found.
+     */
+    private ArrayList<String> errorList;
+
+    /**
+     * Constructor to create a lexical analysis based on an input string.
+     * @param theString String to analyse.
+     */
+    public LexicalAnalyzer(final String theString)
+    {
+        // Initialize state.
+
+        lexicalState = LexicalAnalyzer.lexicalStateStart;
+        stringLocation = 0;
+        stringLength = 0;
+        workString = theString;
+        tokenList = new ArrayList<LexicalToken>();
+        errorList = new ArrayList<String>();
+
+        // If the input string is null, just return - there will be an empty token list and an empty error list.
+
+        if(theString == null)
+            return;
+
+        // Get the length of the string for future use.
+
+        stringLength = workString.length();
+
+        // Perform the lexical analysis.
+
+        analyseString();
+    }
+
+    /**
+     * This method performs analysis on the string.
+     */
+    private void analyseString()
+    {
+
+    }
 
 }
